1d449fac7a531b176358a47a8ba0721c6bbacd03
Raoof Agh <32569954+Ra00f1@users.noreply.github.com>
2023-07-28 14:29:24 +0300

    Mnist download done :|

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+from sklearn.datasets import fetch_openml, load_digits
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def Download_MINST():
+    mnist = fetch_openml('mnist_784')
+    print(mnist.keys())
+    return mnist
+
+def test_mnist(mnist):
+    x, y = mnist["data"], mnist["target"]
+    print(x.shape)
+    print(y.shape)
+
 if __name__ == "__main__":
-    print("yes")
+    mnist = Download_MINST()
+    test_mnist(mnist)
