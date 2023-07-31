fb599d5034cfba94e12dd25d38360017db94fbef
Raoof Agh <32569954+Ra00f1@users.noreply.github.com>
2023-07-31 20:02:25 +0300

    recognizes 5

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from sklearn.datasets import fetch_openml, load_digits
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 from sklearn import datasets, metrics, svm
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import confusion_matrix
 
 def Download_MINST():
     mnist = fetch_openml('mnist_784', parser="auto")
@@ -12,28 +17,31 @@ def Download_MINST():
 
 def test_mnist(mnist):
     x, y = mnist["data"], mnist["target"]
-    print(x.shape)
-    print(y.shape)
-    print(x[:1])
-    image = mnist.data.to_numpy()
-    plt.subplot(431)
-    plt.imshow((image[0].reshape(28, 28)), cmap=plt.cm.gray_r,
-               interpolation='nearest')
-    plt.subplot(432)
-    plt.imshow(image[1].reshape(28, 28), cmap=plt.cm.gray_r,
-               interpolation='nearest')
-    plt.subplot(433)
-    plt.imshow(image[3].reshape(28, 28), cmap=plt.cm.gray_r,
-               interpolation='nearest')
-    plt.subplot(434)
-    plt.imshow(image[4].reshape(28, 28), cmap=plt.cm.gray_r,
-               interpolation='nearest')
-    plt.subplot(435)
-    plt.imshow(image[5].reshape(28, 28), cmap=plt.cm.gray_r,
-               interpolation='nearest')
-    plt.subplot(436)
-    plt.imshow(image[6].reshape(28, 28), cmap=plt.cm.gray_r,
-               interpolation='nearest')
+    #print(x.shape)
+    #print(y.shape)
+
+    y = y.astype(np.uint8)
+    X_train, X_test, y_train, y_test = x[:60000],x[60000:], y[:60000], y[60000:]
+
+    #simple test case (5 only)
+    y_train_5 = (y_train == 5)
+    y_test_5 = (y_test == 5)
+    sgd_clf = SGDClassifier(random_state=42)
+    sgd_clf.fit(X_train, y_train_5)
+
+    print(cross_val_score(sgd_clf, X_train, y_train_5, cv=3, scoring="accuracy"))
+    y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
+    print(confusion_matrix(y_train_5, y_train_pred))
+
+
+    #See the image
+
+    #print(x[3:4].values.reshape((28,28)))
+    #test_digit = x[:1]
+    #test_digit_image = test_digit.values.reshape((28,28))
+    #plt.imshow(test_digit_image, cmap=mpl.cm.binary, interpolation="nearest")
+    #plt.axis("off")
+    #plt.show()
 
 if __name__ == "__main__":
     mnist = Download_MINST()
