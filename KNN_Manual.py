405b937fb0075824ca1e2761fede089763f4d3be
Raoof Agh <32569954+Ra00f1@users.noreply.github.com>
2023-08-07 17:34:32 +0300

    Faster
    KNN_Manual uses multiprocessing and is 41% faster than the KNN_Manual_Version0 which is fine and works perfectly but slower
    
    However KNN_Manual has a problem with adding the results to df (Unsolved)

--- KNN_Manual.py
+++ KNN_Manual.py
@@ -1,29 +1,46 @@
+import time
 import  pandas as pd
 import numpy as np
+import multiprocessing
+
+def calculate_distance(args):
+    array, chunk = args
+    print(chunk)
+    dist = []
+    start = time.time()
+    for i in chunk:
+        if i%100 == 0:
+            end = time.time()
+            print(i,"time: ", end - start)
+            start = time.time()
+        dist = [np.sqrt(np.sum((array[i] - array) ** 2, axis=1))]
+    df = pd.DataFrame(dist, columns=['index', 'distance'])
+    print(df)
+    return df
 
 def Euclidean_Distance(data):
-    train_distance_list = []
-    train_id_counter = []
-    length = len(data)
+    #data = data.head(100)
     array = data.to_numpy()
-
-    import numpy as np
-    import pandas as pd
-
-    # Assuming 'array' is a NumPy array with shape (length, num_features)
     length = array.shape[0]
 
     # Initialize the lists to store distances and indices
     train_distance_list = []
     train_id_counter = []
+    num_processes = 10
 
-    for i in range(length):
-        print(i/length*100,"%")
+    # Split the array indices into chunks for multiprocessing
+    chunks = np.array_split(np.arange(length), num_processes)
+    arguments = [(array, chunk) for chunk in chunks]
 
-        # Calculate the Euclidean distance between array[i] and all other elements in 'array'
-        distance = np.sqrt(np.sum((array[i] - array) ** 2, axis=1))
-        train_distance_list.extend(distance)
-        train_id_counter.extend([i] * length)
+    with multiprocessing.Pool(processes=10) as pool:
+        results = pool.map(calculate_distance, arguments)
+
+    for chunk, distances in zip(chunks, results):
+        print(chunk, "/", length)
+
+        # Append the distances and corresponding indices to the lists
+        train_distance_list.extend(distances)
+        train_id_counter.extend([i for i in chunk] * length)
 
     d = {'index': train_id_counter, 'distance': train_distance_list}
     df = pd.DataFrame(d, columns=['index', 'distance'])
---
+++ KNN_Manual_Version0.py
@@ -0,0 +1,49 @@
+import  pandas as pd
+import numpy as np
+import time
+
+def Euclidean_Distance(data):
+    train_distance_list = []
+    train_id_counter = []
+    length = len(data)
+    array = data.to_numpy()
+    # Assuming 'array' is a NumPy array with shape (length, num_features)
+    length = array.shape[0]
+
+    # Initialize the lists to store distances and indices
+    train_distance_list = []
+    train_id_counter = []
+    start = time.time()
+
+    for i in range(length):
+        if i%100 == 0:
+            end = time.time()
+            print(i,"time: ", end - start)
+            start = time.time()
+
+        # Calculate the Euclidean distance between array[i] and all other elements in 'array'
+        distance = np.sqrt(np.sum((array[i] - array) ** 2, axis=1))
+        train_distance_list.extend(distance)
+        train_id_counter.extend([i] * length)
+
+    d = {'index': train_id_counter, 'distance': train_distance_list}
+    df = pd.DataFrame(d, columns=['index', 'distance'])
+    df_sorted = df.sort_values(by='index')
+
+    print(df_sorted)
+    return df_sorted
+
+def Loading_Data():
+    print("Loading Data!")
+    X_train = pd.read_csv("X_train", sep=',')
+    X_test = pd.read_csv("X_test", sep=',')
+    y_train = pd.read_csv("y_train", sep=',')
+    y_test = pd.read_csv("y_test", sep=',')
+    print("Loading Completed!")
+
+    return X_train, X_test, y_train, y_test
+
+if __name__ == "__main__":
+    X_train, X_test, y_train, y_test = Loading_Data()
+
+    Euclidean_Distance(X_train)
