b8b5103c960113d412a3240861479dbe0ae1f6dc
Raoof Agh <32569954+Ra00f1@users.noreply.github.com>
2023-08-14 15:15:10 +0300

    added some test(temporary)
    Have to add add to dataframe for each number and clear list

--- KNN_Manual_Version0.py
+++ KNN_Manual_Version0.py
@@ -2,9 +2,13 @@ import  pandas as pd
 import numpy as np
 import time
 from collections import Counter
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 def Euclidean_Distance(data):
-    data = data[:100]
+    data = data[:1000]
+    print("Calculating Euclidean Distance!")
+    # Convert the data frame to a NumPy array
     array = data.to_numpy()
     # Assuming 'array' is a NumPy array with shape (length, num_features)
     length = array.shape[0]
@@ -13,6 +17,8 @@ def Euclidean_Distance(data):
     train_distance_list = []
     train_id_counter = []
     start = time.time()
+
+    df = pd.DataFrame(columns=['index', 'distance'])
 
     for i in range(length):
         if i%100 == 0:
@@ -46,5 +52,18 @@ if __name__ == "__main__":
     X_train, X_test, y_train, y_test = Loading_Data()
 
     distance_df = Euclidean_Distance(X_train)
+
+    # Save Data Frame
+    #distance_df.to_csv("Distance_DF", sep=',', index=False, encoding='utf-8')
+
+    # Load Distance Data Frame
+    #distance_df = pd.read_csv("Distance_DF", sep=',')
+    #
+    for i in range(10):
+        test_digit = X_train[distance_df[i:i+1].index.values[0]: distance_df[i:i+1].index.values[0]+1]
+        test_digit_image = test_digit.values.reshape((28, 28))
+        plt.imshow(test_digit_image, cmap=mpl.cm.binary, interpolation="nearest")
+        plt.axis("off")
+        plt.show()
 
     #counter = Counter(y_train[distance_df.index])
